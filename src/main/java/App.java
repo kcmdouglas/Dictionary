192417cc5c2eedda6ffe341e2637ec001caf9be0
Kassidy Douglas <kcmdouglas@gmail.com>
2016-01-15 12:00:35 -0800

    Add Integration tests for most basic pages.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,6 @@
 import java.util.Map;
 import java.util.HashMap;
+import java.util.ArrayList;
 
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
@@ -11,11 +12,71 @@ public class App {
     String layout = "templates/layout.vtl";
 
     get("/", (request, response) -> {
-      Map<String, Object> model = new HashMap<String, Object>();
+      HashMap<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/home.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/words", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("words", Word.all());
+      model.put("template", "templates/words.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/words/new", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/word-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/words", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String word = request.queryParams("word");
+      Word newWord = new Word(word);
+      model.put("word", word);
+      model.put("template", "templates/success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("words/:id/definitions/new", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+
+      Word word = Word.find(Integer.parseInt(request.params(":id")));
+      model.put("word", word);
+      model.put("template", "templates/word-def-form.vtl");
+      return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+
+    post("/definitions", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Word word = Word.find(Integer.parseInt(request.queryParams("wordId")));
+
+      ArrayList<Definition> definitions = word.getDefinitions();
+
+      if (definitions == null) {
+        definitions = new ArrayList<Definition>();
+        request.session().attribute("definitions", definitions);
+      }
+
+      String definition = request.queryParams("definition");
+      Definition newDefinition = new Definition(definition);
+
+      word.addDefinition(newDefinition);
+      model.put("word", word);
+      model.put("template", "templates/singleword.vtl");
+      return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+
+    get("words/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      
+      Word word = Word.find(Integer.parseInt(request.params(":id")));
+      model.put("word", word);
+      model.put("template", "templates/singleword.vtl");
+      return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+
+
   }
 }
-	
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -6,15 +6,18 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.fluentlenium.assertj.FluentLeniumAssertions.assertThat;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
 
 import org.junit.*;
 import static org.junit.Assert.*;
 
 public class AppTest extends FluentTest {
   public WebDriver webDriver = new HtmlUnitDriver();
+
+  @Override
   public WebDriver getDefaultDriver() {
-    return webDriver;
-  }
+   return webDriver;
+ }
 
   @ClassRule
   public static ServerRule server = new ServerRule();
@@ -22,6 +25,34 @@ public class AppTest extends FluentTest {
   @Test
     public void rootTest() {
       goTo("http://localhost:4567/");
-      assertThat(pageSource()).contains("");
+      assertThat(pageSource()).contains("Your Very Own Customizable Dictionary");
+    }
+  @Test
+    public void wordIsCreatedTest() {
+      goTo("http://localhost:4567/");
+      click("a", withText("Add a New Word"));
+      fill("#word").with("Limn");
+      submit(".btn");
+      assertThat(pageSource()).contains("Your new word has been carefully inscribed into your dictionary.");
+    }
+
+  @Test
+    public void wordIsDisplayedTest() {
+      goTo("http://localhost:4567/words/new");
+      fill("#word").with("Limn");
+      submit(".btn");
+      click("a", withText("View Your Words"));
+      assertThat(pageSource()).contains("Limn");
+    }
+
+  @Test
+    public void wordDefinitionFormIsDisplayed() {
+      goTo("http://localhost:4567/words/new");
+      fill("#word").with("Loam");
+      submit(".btn");
+      click("a", withText("View Your Words"));
+      click("a", withText("Loam"));
+      click("a", withText("Add a New Definition"));
+      assertThat(pageSource()).contains("Add a New Definition for Loam");
     }
 }
